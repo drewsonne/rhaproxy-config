c2a3efab81b0e4f1634e2ab2da249523215dcb5b
Drew J. Sonne <drews@blinkbox.com>
2015-02-13 15:16:04 +0000

    Formatted 'get' action on backend to match hap configs.

--- lib/rhaproxy/config/command/backend.rb
+++ lib/rhaproxy/config/command/backend.rb
@@ -7,14 +7,29 @@ module Rhaproxy
       class Backend < Base
 
         def show(*args)
-          print_array_attr('backends', @haproxy_config.backends, 'name')
+          get_array_attr('backends', @haproxy_config.backends, 'name')
         end
 
         def get(args)
           backend_name = args.pop
           backend = @haproxy_config.backend(backend_name)
-          renderer = HAProxy::Renderer.new(backend, backend.options)
-          renderer.render
+
+          # Render Options
+          output = "\nbackend #{backend_name}\n"
+          output += backend.config.map {|config_name, config_value|
+            "#{INDENTATION}#{config_name} #{config_value}"
+          }.join("\n") + "\n"
+          output += backend.options.map {|option_name, option_value|
+            "#{INDENTATION}#{option_name} #{option_value}"
+          }.join("\n")
+          output += backend.servers.map {|server_name, server_data|
+            "#{INDENTATION}server #{server_name} " +
+            "#{server_data.host}:" +
+            "#{server_data.port} " +
+            server_data.attributes.map { |attribute_key, attribute_value|
+              attribute_key if attribute_value
+            }.join(", ")
+          }.join("\n")
         end
 
       end
