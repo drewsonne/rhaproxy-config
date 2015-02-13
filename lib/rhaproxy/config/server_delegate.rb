f867ea25e3bbeda8b57b0d106a9b8f3caa374918
Drew J. Sonne <drews@blinkbox.com>
2015-02-13 18:06:00 +0000

    Added interactive mode.

--- lib/rhaproxy/config/server_delegate.rb
+++ lib/rhaproxy/config/server_delegate.rb
@@ -5,6 +5,7 @@ module Rhaproxy
     class ServerDelegate < EventMachine::Connection
 
       @haproxy_config
+      @is_interactive = false
 
       def initialize(haproxy_config)
         @haproxy_config = haproxy_config
@@ -17,21 +18,25 @@ module Rhaproxy
       def receive_data(data, prompt=false)
         first_line_command = data.split(/[\r\n]+/)[0]
         action, resource, *arguments = first_line_command.split(/(?<!\\) /) # Split tokens by unescaped strings
-        if action == 'prompt'
-          response_with_prompt
-        else
-          case action
-            when *%w{get set show}
-              resource = resource.gsub(/s$/, '')
-              send_data find_object(resource).send(action, arguments) + "\n"
-            else
-              raise "Unexpected action '#{action}'."
-          end
+        case action
+          when *%w{get set show}
+            resource = resource.gsub(/s$/, '')
+            send_data find_object(resource).send(action, arguments)
+            close_connection_after_writing if not @is_interactive
+          when "prompt"
+            respond_with_prompt
+          when "quit"
+            close_connection
+          else
+            raise "Unexpected action '#{action}'."
         end
+
+        send_data "\n> "
+
       end
 
-      def response_with_prompt
-        send_data "> "
+      def respond_with_prompt
+        @is_interactive = true
       end
 
       def find_object(resource_name)
